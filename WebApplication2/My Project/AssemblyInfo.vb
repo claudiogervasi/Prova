1aff830b9c3df04c4b1adb04ae2a24157cebd09a
claudio <claudio.gervasi@gmail.com>
2012-09-14 12:38:14 +0200

    a

---
+++ WebApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Rivedere i valori degli attributi dell'assembly
+<Assembly: AssemblyTitle("WebApplication2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("WebApplication2")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della libreria dei tipi
+<Assembly: Guid("6528c79a-7d7b-4ed3-a3de-20117b774b06")> 
+
+' Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+'
+'      Versione principale
+'      Versione secondaria 
+'      Numero build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare come predefiniti i valori Numero revisione e Numero build 
+' utilizzando l'asterisco (*) come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
